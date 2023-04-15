0f31df8ef61b09a2d2ece4be23ad3562ea96f68c
serirohan <rohanseri9@gmail.com>
2023-04-15 23:24:33 +0530

    three commit

---
+++ src/Compounets/Index.jsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+//  import{useNavigate}from'react-router-dom';
+
+
+import "./App.css";
+
+
+function Index() {
+    const initialValues = { username: "", email: "", password: "" };
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+  
+    // const handleChange = (e) => {
+    //   const { name, value } = e.target;
+    //   setFormValues({ ...formValues, [name]: value });
+    // };
+  
+    const  handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(e.taget.username.value);
+    //   e.preventDefault();
+    //   setFormErrors(validate(formValues));
+    //   setIsSubmit(true);
+    };
+  
+    // useEffect(() => {
+    //   console.log(formErrors);
+    //   if (Object.keys(formErrors).length === 0 && isSubmit) {
+    //     console.log(formValues);
+    //   }
+    // }, [formErrors]);
+    // const validate = (values) => {
+    //   const errors = {};
+    //   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    //   if (!values.username) {
+    //     errors.username = "Username is required!";
+    //   }
+    //   if (!values.email) {
+    //     errors.email = "Email is required!";
+    //   } else if (!regex.test(values.email)) {
+    //     errors.email = "This is not a valid email format!";
+    //   }
+    //   if (!values.password) {
+    //     errors.password = "Password is required";
+    //   } else if (values.password.length < 4) {
+    //     errors.password = "Password must be more than 4 characters";
+    //   } else if (values.password.length > 10) {
+    //     errors.password = "Password cannot exceed more than 10 characters";
+    //   }
+    //   return errors;
+    // };
+
+
+  return (
+    <div className="container">
+
+
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              id='username'
+            //   value={formValues.username}
+            //   onChange={handleChange}
+            />
+          </div>
+          {/* <p>{formErrors.username}</p> */}
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              id='email'
+            //   value={formValues.email}
+            //   onChange={handleChange}
+            />
+          </div>
+          {/* <p>{formErrors.email}</p> */}
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id='password'
+            //   value={formValues.password}
+            //   onChange={handleChange}
+            />
+          </div>
+          {/* <p>{formErrors.password}</p> */}
+          <input type='sumbit' value='Dashboard' id='sumbit' />
+        </div>
+      </form>
+    </div>
+    
+  )
+}
+
+export default Index
